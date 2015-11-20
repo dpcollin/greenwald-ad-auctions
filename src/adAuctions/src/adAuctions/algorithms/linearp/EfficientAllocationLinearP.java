92c3de8966e94408cae253d06acee9fea713b176
Enrique Areyan Viqueira <eareyanv@cs.brown.edu>
2015-11-20 15:43:35 -0500

    getting ALL efficient allocations

--- src/adAuctions/src/adAuctions/algorithms/linearp/EfficientAllocationLinearP.java
+++ src/adAuctions/src/adAuctions/algorithms/linearp/EfficientAllocationLinearP.java
@@ -1,5 +1,7 @@
 package adAuctions.algorithms.linearp;
 
+import java.util.ArrayList;
+
 import adAuctions.structures.Market;
 import ilog.concert.IloException;
 import ilog.concert.IloLinearNumExpr;
@@ -9,7 +11,10 @@ import ilog.cplex.IloCplex;
 
 
 public class EfficientAllocationLinearP {
-
+	/*
+	 * Boolean to control whether or not to output.
+	 */
+	protected boolean verbose = false;
 	/*
 	 * The market for which we are going to build the linear
 	 * programming solution
@@ -26,26 +31,37 @@ public class EfficientAllocationLinearP {
 		this.market = G;
 	}
 
-	public double[][] Solve(){
+	public ArrayList<double[][]> Solve(){
 		try {
 			this.cplex = new IloCplex();
-			//variables
+			if(!this.verbose) cplex.setOut(null);
+			/*
+			 * These two next parameters controls how many solutions we want to get.
+			 * The first parameter controls how far from the optimal we allow solutions to be,
+			 * The second parameter controls how many solutions we will get in total.
+			 */
+			this.cplex.setParam(IloCplex.DoubleParam.SolnPoolGap, 0.0);
+			this.cplex.setParam(IloCplex.IntParam.PopulateLim, 1000);
+			/*
+			 * variables
+			 */
 			IloNumVar[] y = cplex.boolVarArray(this.market.getNumberCampaigns());
-
 			IloNumVar[][] u = new IloNumVar[this.market.getNumberCampaigns()][];
 			for (int i=0; i<this.market.getNumberCampaigns(); i++){
 				u[i] = cplex.intVarArray(this.market.getNumberUsers(),0,Integer.MAX_VALUE);
 			}
-			
-			// Objective
+			/*
+			 * Objective.
+			 */
 			IloLinearNumExpr obj = this.cplex.linearNumExpr();
 			for (int i=0; i<this.market.getNumberCampaigns(); i++){
 				obj.addTerm(this.market.campaigns[i].totalValue, y[i]);
 			}
 			this.cplex.addMaximize(obj);
 			
-			//constraints
-			
+			/*
+			 * Constraints.
+			 */
 			for (int i=0; i<this.market.getNumberCampaigns(); i++){
 				double coeff = 1.0/(double)this.market.campaigns[i].numImpressions;
 				IloLinearNumExpr expr = cplex.linearNumExpr();
@@ -62,8 +78,6 @@ public class EfficientAllocationLinearP {
 				this.cplex.addGe(expr,y[i]);
 				this.cplex.addLe(expr,1);
 			}
-			
-			
 			for (int j=0; j<this.market.getNumberUsers(); j++){
 				IloLinearNumExpr expr = cplex.linearNumExpr();
 				for (int i=0; i<this.market.getNumberCampaigns(); i++){
@@ -71,22 +85,37 @@ public class EfficientAllocationLinearP {
 				}
 				this.cplex.addLe(expr,this.market.userSets[j].numUsers);
 			}
-			
+			/*
+			 * Solve the problem and get many solutions:
+			 */
 			this.cplex.solve();
-			double[][] out = new double[this.market.getNumberCampaigns()][this.market.getNumberUsers()];
-			for (int i=0; i<this.market.getNumberCampaigns(); i++){
-				out[i] = this.cplex.getValues(u[i]);
-			}
-			
-			for (int i=0; i<this.market.getNumberUsers(); i++){
-				for (int j=0; j<this.market.getNumberCampaigns(); j++){
-					System.out.print(out[j][i]);
-					System.out.print(" ");
+			if ( cplex.populate() ) {
+				int numsol = cplex.getSolnPoolNsolns();
+	            int numsolreplaced = cplex.getSolnPoolNreplaced();
+				/*
+				 * Print some information
+				 */
+				if(this.verbose){
+					System.out.println("***********************************Populate");
+		            System.out.println("The solution pool contains " + numsol + " solutions.");
+		            System.out.println(numsolreplaced + " solutions were removed due to the " + "solution pool relative gap parameter.");
+		            System.out.println("In total, " + (numsol + numsolreplaced) + " solutions were generated.");
 				}
-				System.out.print("\n");
+	            /*
+	             * Store all the solutions in an ArrayList.
+	             */
+	            ArrayList<double[][]> Solutions = new ArrayList<>();
+	            for (int l = 0; l < numsol; l++) {
+	    			double[][] sol = new double[this.market.getNumberCampaigns()][this.market.getNumberUsers()];
+	    			for (int i=0; i<this.market.getNumberCampaigns(); i++){
+	    				sol[i] = this.cplex.getValues(u[i],l);
+	    			}
+	    			Solutions.add(sol);
+	            }
+	            this.cplex.end();
+	            return Solutions;
 			}
-			System.out.print(this.cplex.getObjValue());
-			
+			return null;
 		} catch (IloException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Exception: ==>");
--- src/adAuctions/src/adAuctions/algorithms/linearp/test/EfficientAllocationTesting.java
+++ src/adAuctions/src/adAuctions/algorithms/linearp/test/EfficientAllocationTesting.java
@@ -1,16 +1,74 @@
 package adAuctions.algorithms.linearp.test;
 
+import java.util.ArrayList;
+
 import adAuctions.algorithms.linearp.EfficientAllocationLinearP;
+import adAuctions.structures.Campaign;
+import adAuctions.structures.Connection;
 import adAuctions.structures.Market;
+import adAuctions.structures.UserSet;
 
 
 public class EfficientAllocationTesting{
 	public static void main(String[] args){
-		Market M = new Market(3,3,1,100,1,1,4,4,1,1,0.5);
+		Market M = new Market(3,3,1,100,1,1,3,3,1,1,1.0);
 		M.setConnectionsMatrix();
 		EfficientAllocationLinearP eff = new EfficientAllocationLinearP(M);
-		eff.Solve();
+		ArrayList<double[][]> efficientAllocations = eff.Solve();
 		System.out.println("\n" + M);
+		System.out.println("We found "+efficientAllocations.size()+" many efficient allocations");
+		
+		for(int l=0;l<efficientAllocations.size();l++){
+			System.out.println("Solution #"+l);
+			for (int i=0; i<M.getNumberUsers(); i++){
+				for (int j=0; j<M.getNumberCampaigns(); j++){
+					System.out.print(efficientAllocations.get(l)[j][i]);
+					System.out.print(" ");
+				}
+				System.out.print("\n");
+			}
+		}		
+		
+		/*
+		 * The following test is for a market with some number of users,
+		 * some number of campaigns where all of them are connected.
+		 */
+		/*
+		int numCam = 10;
+		int numUse = 10;
+		Connection[] Connections = new Connection[numCam*numUse];
+		UserSet[] U = new UserSet[numUse];
+		int k=0;
+		for(int i=0;i<numUse;i++){
+			U[i] = new UserSet(1);
+			for(int j=0;j<numCam;j++){
+				Connections[k] = new Connection(i,j);
+				k++;
+			}
+		}
+		Campaign[] C = new Campaign[numCam];
+		for(int j=0;j<numCam;j++){
+			C[j] = new Campaign(1,(j+1)*10);
+		}
+		Market F = new Market(C, U, Connections);
+		F.setConnectionsMatrix();
+		EfficientAllocationLinearP eff = new EfficientAllocationLinearP(F);
+		ArrayList<double[][]> efficientAllocations = eff.Solve();
+		System.out.println(F);
+		System.out.println("We found "+efficientAllocations.size()+" many efficient allocations");
+		
+		for(int l=0;l<efficientAllocations.size();l++){
+			System.out.println("Solution #"+l);
+			for (int i=0; i<numUse; i++){
+				for (int j=0; j<numCam; j++){
+					System.out.print(efficientAllocations.get(l)[j][i]);
+					System.out.print(" ");
+				}
+				System.out.print("\n");
+			}
+		}*/		
+		
+		
 	}
 }
 
